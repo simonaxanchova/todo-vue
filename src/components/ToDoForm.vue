75d406de822a7e40363ebe0aea958728bd65a5e8
Simona Anchova <anchovasimona@yahoo.com>
2021-01-30 13:48:45 +0100

    New files added

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <h2 class="label-wrapper">
+        <label for="new-todo-input" class="label__lg">
+            What Needs To Be Done?
+        </label>
+        </h2>
+        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model="label" class="input__lg">
+        <button type="submit" class="btn btn__primary btn__lg">Add</button>
+    </form>
+</template>
+
+<script>    
+    export default {
+        methods: {
+            onSubmit() {
+                if(this.label === '') // ne dozvoluva da se pojavi item pri submit na prazno pole
+                    return;
+                this.$emit("todo-added", this.label)
+                this.label = '' // po dodavanjeto na nov element da se brishe input poleto
+            }
+        },
+        data() {
+            return {
+                label: ""
+            }
+        }
+    };
+</script>
